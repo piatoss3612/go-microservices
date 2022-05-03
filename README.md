a388495422bfa17617fef3de5679383e2cf9cd4e
Hyohwak Lee <crewe1746@naver.com>
2022-05-03 18:28:59 +0900

    Update broker service and connect to auth service

--- README.md
+++ README.md
@@ -4,57 +4,7 @@
 
 ### 1. Broker Service
 
-1. broker service 생성
-
-- 라우터: `github.com/go-chi/chi/v5`
-- 미들웨어: `github.com/go-chi/chi/v5/middleware`
-- CORS: `github.com/go-chi/cors`
-  <br>
-
-2. Docker 이미지 빌드
-
-- `broker-service.dockerfile`
-- `docker-compse.yml`
-- 도커 서버 실행 & `docker-compose up -d`
-
-3. broker service 테스트
-
-- `localhost:80`을 사용할 수 없었던 문제 -> 아파치 서버를 종료함으로써 해결
-- 도커 컨테이너 재실행
-- 테스트 코드 작성
-- `go run ./cmd/web`
-
-4. JSON 형식의 데이터 처리를 도와주는 helper 함수 추가
-
-- readJSON, writeJSON, errorJSON
-
-5. make, Makefile을 사용하여 컴파일 과정 단순화
-
-- Makefile: 컴파일에 필요한 명령어를 묶어 `make [커스텀 명령어]` 형식으로 실행
-- Error: `Makefile: *** missing separator. Stop.`
-  - Make 파일에서 tab을 공백\*4로 이해하는 문제 해결 방법
-    - vscode -> command palette -> Convert Indentation to Tabs
-
-### Auth Service
-
-> User -요청-> Broker -요청전달-> Auth -> DB -> Auth -응답-> Broker -응답전달-> User
-
-1. authentication service 구현
-
-- 사용자 데이터 모델 추가
-- 라우터 추가
-
-2. 데이터 베이스 연동 코드 작성
-
-- `go get github.com/jackc/pgconn`
-- `go get github.com/jackc/pgx/v4`
-
-3. Docker 이미지 빌드
-
-- `authentication-service.dockerfile`
-- `docker-compse.yml` 파일에 authentication-service, postgres 항목 추가
-
-4. 라우트 & 핸들러 추가
+### 2. Auth Service
 
 ---
 
--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,8 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"net/http"
 )
+
+// broker는 다른 마이크로 서비스들의 single point of entry이므로
+// 합의된 데이터 형식을 정의하여 이를 따르도록 한다
+type RequestPayload struct {
+	Action string      `json:"action"`
+	Auth   AuthPayload `json:"auth,omitempty"`
+}
+
+type AuthPayload struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
@@ -11,4 +26,72 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
+	var requestPayload RequestPayload
+
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	switch requestPayload.Action {
+	case "auth":
+		app.authenticate(w, requestPayload.Auth)
+	default:
+		app.errorJSON(w, errors.New("unkown action"))
+	}
+}
+
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
+	// create some json, then send it to the auth microservice
+	jsonData, _ := json.MarshalIndent(a, "", "\t")
+
+	// call the service
+	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		app.writeJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	// make sure we get back correct status code
+	if response.StatusCode == http.StatusUnauthorized {
+		app.errorJSON(w, errors.New("invalid credentials"))
+		return
+	} else if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, errors.New("error calling auth service"))
+		return
+	}
+
+	// create a variable we'll read response.Body into
+	var jsonFromService jsonResponse
+
+	// decode the json from the auth service
+	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	if jsonFromService.Error {
+		app.errorJSON(w, err, http.StatusUnauthorized)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "Authenticated!"
+	payload.Data = jsonFromService.Data
+
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
